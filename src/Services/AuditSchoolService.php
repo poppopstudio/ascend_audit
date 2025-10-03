feb118237317127c2a8629ba8d86585a134f372e
NikLP <NikLP@users.noreply.github.com>
2025-10-03 16:45:14 +0100

    Mods to workingschool service

--- src/Services/AuditSchoolService.php
+++ src/Services/AuditSchoolService.php
@@ -22,8 +22,8 @@ class AuditSchoolService {
       /**
        * Could do something a bit more impactful but user shouldn't have perms
        * to access anything that requires the value?
-      */
-      return null; // 1??
+       */
+      return 1; // was null but...
     }
 
     // Process matching roles (maintains priority order from $profile_roles).
@@ -37,21 +37,18 @@ class AuditSchoolService {
       if ($profile && !$profile->get('ascend_p_school')->isEmpty()) {
         return $profile->get('ascend_p_school')->target_id;
       }
-
-      // At this point, user does not have a saved profile entry.
-      $message_vars = [
-        '@user' => $current_user->getDisplayName(),
-        // '@additional' => implode(', ', $roles_to_add),
-      ];
-
-      \Drupal::logger('ascend_audit')->warning('User @user does not have a working school assigned in their profile', $message_vars);
-      \Drupal::messenger()->addWarning(t('Please set a working school in your profile', $message_vars));
-
-      // Return at least a token value in order to not break things.
-      return 1; // Will be the ID of the first available school.
     }
 
-    return;
+    // At this point, user does not have a saved profile entry.
+    $message_vars = [
+      '@user' => $current_user->getDisplayName(),
+    ];
+
+    \Drupal::logger('ascend_audit')->warning('User @user does not have a working school assigned in their profile', $message_vars);
+    \Drupal::messenger()->addWarning(t('Please set a working school in your profile to continue.', $message_vars));
+
+    // Return at least a token value in order to not break things.
+    return 1; // Will be the ID of the first available school.
   }
 
   /**
@@ -61,12 +58,19 @@ class AuditSchoolService {
     // Given we can get the ID from the other service function...
     $school_id = $this->getWorkingSchool();
 
+    if (!isset($school_id)) {
+      return;
+    }
+
     // We can get the school name from here.
     $school_entity = \Drupal::entityTypeManager()
       ->getStorage('school')
       ->load($school_id);
 
+    if (!isset($school_entity)) {
+      return;
+    }
+    
     return $school_entity->label();
   }
-
 }
