c072ef15292c26b41bec6c604cddc63a6f29094c
Marek P <marekpercz@gmail.com>
2026-02-11 11:13:56 +0000

    Auditor + Adviser conflict validation disabled.

--- src/Plugin/Validation/Constraint/AuditRoleConflictValidator.php
+++ src/Plugin/Validation/Constraint/AuditRoleConflictValidator.php
@@ -27,10 +27,11 @@ class AuditRoleConflictValidator extends ConstraintValidator {
 
     // Check for conflicting roles.
 
-    // Auditor/adviser cannot co-exist, privacy issue.
-    if (in_array('auditor', $roles) && in_array('adviser', $roles)) {
-      $this->context->addViolation($constraint->auditor_adviser_conflict);
-    }
+    //    // Auditor/adviser cannot co-exist, privacy issue.
+    //    if (in_array('auditor', $roles) && in_array('adviser', $roles)) {
+    //      $this->context->addViolation($constraint->auditor_adviser_conflict);
+    //    }
+    // commented out on 11 Feb. This validation is not required atm.
 
     // Audit manager/adviser cannot co-exist, privacy issue.
     if (in_array('audit_manager', $roles) && in_array('adviser', $roles)) {
